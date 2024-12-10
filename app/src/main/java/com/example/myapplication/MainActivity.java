cac9279d8e4b2a70c05bfb99a15a2c93b4277ada
Huuwi <110718832+Huuwi@users.noreply.github.com>
2024-12-10 10:15:34 +0700

    duc

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -19,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         deleteDatabase("app_database.db");
-        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(loginIntent);
+        SharedPreferences mySharedPrefer = getSharedPreferences("mySharedPrefer", MODE_PRIVATE);
+
+        int userID = mySharedPrefer.getInt("userID" , -1);
+        if(userID <= 0){
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+        }else {
+
+        }
+
+
     }
 }
